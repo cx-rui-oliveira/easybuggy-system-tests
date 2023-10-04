11bcc37aa37b0100aeb2cb07e8a700aace9f2f8e
Diogo-fj-rocha <diogo.fj.rocha@gmail.com>
2023-10-04 16:38:10 +0100

    restore secret. Remove possible future secrets

--- secrets.go
+++ secrets.go
@@ -2,8 +2,5 @@ package easybuggy4sb
 
 import os
 
-// Sample secrets
-api_key = "sk_test_0123456789abcdef0123456789abcdef"
-password = "mySuperSecretPassword"
 database_url = "postgresql://username:password@localhost/mydatabase"
 aws_access_key = "AKIA1234567890EXAMPLE"
